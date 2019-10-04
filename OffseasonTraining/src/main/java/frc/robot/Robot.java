63e9f720918d8655051d2fb04b147d61d663e574
Shadowguy5 <47903245+Shadowguy5@users.noreply.github.com>
2019-10-03 19:25:50 -0500

    Hatch Subsystem
    Minor changes to Robot.java
    Added HatchSubsystem.java
    Removed Herobrine

--- OffseasonTraining/src/main/java/frc/robot/Robot.java
+++ OffseasonTraining/src/main/java/frc/robot/Robot.java
@@ -9,7 +9,6 @@ package frc.robot;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.subsystems.DriveSubsystem;
