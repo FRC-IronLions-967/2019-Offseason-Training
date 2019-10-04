4aec60084d67fe2779031a87489f1ab62f6f8ed7
Shadowguy5 <47903245+Shadowguy5@users.noreply.github.com>
2019-10-03 20:07:19 -0500

    Controls
    Added buttons

--- OffseasonTraining/src/main/java/frc/robot/IO.java
+++ OffseasonTraining/src/main/java/frc/robot/IO.java
@@ -8,6 +8,9 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import frc.commands.grabber.*;
+import frc.commands.arm.*;
 
 /**
  * Add your docs here.
@@ -22,6 +25,20 @@ public class IO {
     public IO() {
         xbox0 = new Joystick(0);
         xbox1 = new Joystick(1);
+        JoystickButton xbox1_a = new JoystickButton(xbox1, 1);
+        JoystickButton xbox1_b = new JoystickButton(xbox1, 2);
+        JoystickButton xbox1_x = new JoystickButton(xbox1, 3);
+        JoystickButton xbox1_y = new JoystickButton(xbox1, 4);
+    
+        xbox1_a.whenPressed(new ArmUp());
+        xbox1_b.whenPressed(new ArmDown());
+        xbox1_x.whenPressed(new GrabberOpenCommand());
+        xbox1_y.whenPressed(new GrabberCloseCommand());
+        
+        xbox1_a.close();
+        xbox1_b.close();
+        xbox1_x.close();
+        xbox1_y.close();
     }
 
     public Joystick getXbox0() {
