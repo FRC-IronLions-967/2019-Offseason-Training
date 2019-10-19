118acdbbf80175d61da9eb247044ccb0a2d1c360
nastark123 <43659552+nastark123@users.noreply.github.com>
2019-10-19 08:52:09 -0500

    Added manipulator subsystems, commands, and IO class

---
+++ RewiringCode/RewiringCode/src/main/java/frc/robot/subsystems/CargoSubsystem.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+/**
+ * Add your docs here.
+ */
+public class CargoSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  private VictorSPX roller;
+
+  public CargoSubsystem() {
+    roller = new VictorSPX(0);
+  }
+
+  public void intake(double power) {
+    roller.set(ControlMode.PercentOutput, power);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
